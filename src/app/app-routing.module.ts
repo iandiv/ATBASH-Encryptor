bd3cc689147692d579a7fec68a8739240a60c47b
Ian Div <dvineian1239@gmail.com>
2022-11-18 12:05:25 +0800

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,10 +16,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
-  },
+
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
