623aff356c803f1ce284344c6046c3be5dac2335
Ian Div <dvineian1239@gmail.com>
2022-11-20 20:18:01 +0800

    changes

--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -12,7 +12,7 @@ export class LoaderPage implements OnInit {
 
   ngOnInit() {
     setTimeout(()=>{
-      // this.router.navigate(["home"])
+      this.router.navigate(["home"])
     },2000)
   }
   
